55999111f79fb3e44ff0f7c72e35027f49e63969
pater97 <giuseppepater997@gmail.com>
2021-11-19 14:29:31 +0100

    MILLESTONE 2 COMPLETE

--- src/components/main.vue
+++ src/components/main.vue
@@ -9,11 +9,28 @@
     <!-- container film  -->
     <section class="show_films">
       <div class="films" v-for="film in movies" :key="film.id">
-        <h1>titolo:{{ film.title }}</h1>
-        <h5>lenguage: {{ film.original_language }}</h5>
-        <h3>vote: {{ film.vote_average }}</h3>
+        <!-- titolo -->
+        <div class="title">
+          <h1 v-if="film.title">titolo:{{ film.title }}</h1>
+          <h1 v-else>titolo: {{ film.name }}</h1>
+        </div>
+        <!-- /tiotolo -->
+        <!-- titolo originale -->
+        <div class="original_title">
+          <h2 v-if="film.original_title">
+            titolo originale: {{ film.original_title }}
+          </h2>
+          <h2 v-else>titolo originale: {{ film.original_name }}</h2>
+        </div>
+        <!-- /titolo originale -->
+        <!-- voto -->
+        <div class="vote">
+          <h3>vote: {{ film.vote_average }}</h3>
+        </div>
+        <!-- /voto -->
+        <!-- sezione flag e language -->
         <div class="lenguage">
-          <h2>titolo originale: {{ film.original_title }}</h2>
+          <h5>lenguage: {{ film.original_language }}</h5>
           <country-flag
             v-if="film.original_language == 'en'"
             country="gb"
@@ -30,7 +47,7 @@
             size="normal"
           />
         </div>
-
+        <!-- /sezione flag e language -->
         <hr />
       </div>
     </section>
@@ -48,26 +65,47 @@ export default {
   data() {
     return {
       searchText: "",
+      apiKey:'f4f382566368553eb6b723d584f2ef40&query=',
+      apiFilm:'https://api.themoviedb.org/3/search/movie?api_key=',
+      apiSeries:'https://api.themoviedb.org/3/search/tv?api_key=',
       movies: [],
       en: "https://img.icons8.com/color/48/000000/great-britain-circular.png",
     };
   },
   methods: {
-    // funzione che richiama l'api al click del bottone
-    getCall() {
+    // funzione che richiama l'api al click del bottone sia serie tv che film.
+    getCall(){
+      this.getSeries(this.apiSeries)
+      this.getMovies(this.apiFilm)
+    },
+     getMovies(api) {
       axios
         .get(
-          "https://api.themoviedb.org/3/search/movie?api_key=f4f382566368553eb6b723d584f2ef40&query=" +
-            this.searchText
+          api + this.apiKey + this.searchText
         )
         .then((response) => {
-          this.movies = response.data.results;
+          const singleObject = response.data.results;
+          for (let i = 0; i < singleObject.length; i++) {
+            this.movies.push(singleObject[i]);
+          }
           console.log(this.movies);
         })
         .catch((e) => {
           console.log(e, "ops!");
         });
     },
+     getSeries(api) {
+      axios
+        .get(
+          api + this.apiKey + this.searchText
+        )
+        .then((response) => {
+          this.movies = response.data.results;
+        })
+        .catch((e) => {
+          console.log(e, "ops!");
+        });
+    }
   },
 };
 </script>
